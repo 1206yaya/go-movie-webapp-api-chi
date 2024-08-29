0acb33410a0dcc3a13575b24299acb75f47ef288
1206yaya <1206yaya@gmail.com>
2024-08-29 19:16:13 +0900

    feat: Add GetRefreshCookie and GetExpiredRefreshCookie methods to Auth

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -71,11 +72,39 @@ func (j *Auth) GenerateTokenPair(user *jwtUser) (TokenPairs, error) {
 	}
 
 	// Create TokenPairs and populate with signed tokens
-	var tokenPairs = TokenPairs{
-		Token:        signedAccessToken,
+	var tokenPairs = TokenPairs {
+		Token: signedAccessToken,
 		RefreshToken: signedRefreshToken,
 	}
 
 	// Return TokenPairs
 	return tokenPairs, nil
+}
+
+func (j *Auth) GetRefreshCookie(refreshToken string) *http.Cookie {
+	return &http.Cookie{
+		Name: j.CookieName,
+		Path: j.CookiePath,
+		Value: refreshToken,
+		Expires: time.Now().Add(j.RefreshExpiry),
+		MaxAge: int(j.RefreshExpiry.Seconds()),
+		SameSite: http.SameSiteStrictMode,
+		Domain: j.CookieDomain,
+		HttpOnly: true,
+		Secure: true,
+	}
+}
+
+func (j *Auth) GetExpiredRefreshCookie() *http.Cookie {
+	return &http.Cookie{
+		Name: j.CookieName,
+		Path: j.CookiePath,
+		Value: "",
+		Expires: time.Unix(0, 0),
+		MaxAge: -1,
+		SameSite: http.SameSiteStrictMode,
+		Domain: j.CookieDomain,
+		HttpOnly: true,
+		Secure: true,
+	}
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -16,8 +16,9 @@ func (app *application) routes() http.Handler {
 
 	mux.Get("/", app.Home)
 
-	mux.Get("/authenticate", app.authenticate)
-
+	mux.Post("/authenticate", app.authenticate)
+	mux.Get("/refresh", app.refreshToken)
+	mux.Get("/logout", app.logout)
 	mux.Get("/movies", app.AllMovies)
 
 	return mux
